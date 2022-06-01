50d68fc7c0e3d4a89142002480a4e44aeb09c727
mrajafirdaus <75506913+mrajafds@users.noreply.github.com>
2022-06-02 03:13:20 +0700

    Add files via upload

---
+++ indonesia.php
@@ -0,0 +1,38 @@
+<?php
+
+//menu utama
+define ("language", 'Bahasa');
+define ("home", 'Beranda');
+define ("services", 'Divisi');
+define ("gallery", 'Galleri');
+define ("aboutus", 'Tentang UKM Edelweis');
+define ("contactus", 'Kontak');
+
+//tombol
+define ("moredetail", "Lihat Divisi");
+define ("buttoncontinue", "lihat Galeri Kami");
+define ("buttonsendemail", "Kirim Pesan");
+
+//judul
+define ("labelgallery", "Galeri");
+define ("labelgallerydescription", "Galeri Foto Kegiatan");
+define ("labelservices", "Divisi");
+define ("labelaboutus", "Sekilas Tentang Kami");
+define ("labelcontactus", "Kontak Kami");
+
+//form email
+define ("placename", "Nama Anda...");
+define ("placesubject", "Subjek...");
+define ("placeemail", "Alamat Email Anda...");
+define ("placemessage", "Isi Pesan...");
+define ("buttonsendmail", "Kirim Pesan");
+
+//footer
+define ("labellocation", "Lokasi");
+define ("labelinfo", "Informasi Lengkap");
+define ("labelheadoffice", "Kantor Pusat");
+define ("labelphone", "Telepon");
+define ("labelemail", "Email");
+define ("labelsosmed", "Media Sosial");
+
+?>
